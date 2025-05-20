b2ceb80b8df6e860841581a64885e17b8739e055
Madhu Charan <madhucharan512@gmail.com>
2025-05-20 21:07:32 +0530

    feat: implement menu component and integrate with header

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,10 +1,8 @@
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
-import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import ModeToggle from "./mode-toggle";
+import Menu from "./Menu";
 
 const Header = () => {
   return (
@@ -24,19 +22,7 @@ const Header = () => {
             </span>
           </Link>
         </div>
-        <div className="space-x-2">
-          <ModeToggle />
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingCart /> Cart
-            </Link>
-          </Button>
-          <Button asChild variant="ghost">
-            <Link href="/sign-in">
-              <UserIcon /> Sign In
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   );
