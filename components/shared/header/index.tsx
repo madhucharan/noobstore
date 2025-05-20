08c32865a9f6edf9a3be48c904f18c9e01bdd88e
Madhu Charan <madhucharan512@gmail.com>
2025-05-20 20:36:39 +0530

    feat: add footer and header components with theme toggle functionality

---
+++ components/shared/header/index.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@/components/ui/button";
+import { APP_NAME } from "@/lib/constants";
+import { ShoppingCart, UserIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import ModeToggle from "./mode-toggle";
+
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex-between">
+        <div className="flex-start">
+          <Link className="flex-start" href="/">
+            <Image
+              src="/images/logo.svg"
+              alt={`${APP_NAME} logo`}
+              height={48}
+              width={48}
+              priority={true}
+            />
+            <span className="hidden lg:block font-bold text-2xl ml-3">
+              {APP_NAME}
+            </span>
+          </Link>
+        </div>
+        <div className="space-x-2">
+          <ModeToggle />
+          <Button asChild variant="ghost">
+            <Link href="/cart">
+              <ShoppingCart /> Cart
+            </Link>
+          </Button>
+          <Button asChild variant="ghost">
+            <Link href="/sign-in">
+              <UserIcon /> Sign In
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
