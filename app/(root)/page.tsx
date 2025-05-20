67659f322a1086364b27a500b1eec957cffa0ef2
Madhu Charan <madhucharan512@gmail.com>
2025-05-20 21:54:01 +0530

    feat: implement product listing and card components with sample data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import ProductList from "@/components/product/product-list";
+import sampleData from "@/db/sample-data";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Products</h2>
+      <ProductList
+        title="Newest Arrivals"
+        data={sampleData.products}
+        limit={4}
+      />
+    </div>
+  );
 };
-
 export default HomePage;
